3b192c9872e9998bd914bf37ef5e28fd2bd2c6ad
LeoFvf <89820850+LeoFvf@users.noreply.github.com>
2022-05-11 21:57:13 -0300

    Primeiras modificações

--- ListaOrdenadaDePalavras.java
+++ ListaOrdenadaDePalavras.java
@@ -1,28 +1,67 @@
 /**
  * Esta classe guarda as palavra do indice remissivo em ordem alfabetica.
+ * 
  * @author Isabel H. Manssour
  */
 
 public class ListaOrdenadaDePalavras {
+    private Palavra refHead = null;
 
-    // Classe interna 
+    // Classe interna
     private class Palavra {
-        public String s;
+
+        public String str;
         public ListaDeOcorrencias listaOcorrencias;
-        public Palavra next;    
+        public Palavra next;
+
         public Palavra(String str) {
-            s = str;
+            this.str = str;
             next = null;
             listaOcorrencias = new ListaDeOcorrencias();
         }
-        
-        // Construtores
 
-	// metodo add para adicionar uma palavra na lista
+        public String getStr() {
+            return str;
+        }
+
+        public void setStr(String str) {
+            this.str = str;
+        }
         
-        // metodo toString
-        
+
+        public ListaDeOcorrencias getListaOcorrencias() {
+            return listaOcorrencias;
+        }
+
+        public void setListaOcorrencias(ListaDeOcorrencias listaOcorrencias) {
+            this.listaOcorrencias = listaOcorrencias;
+        }
+
+        public Palavra getNext() {
+            return next;
+        }
+
+        public void setNext(Palavra next) {
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "Palavra [str=" + str + "]";
+        }
+
+        // metodo add para adicionar uma palavra na lista
+
         // demais metodos necessarios
     }
-    
+
+    // Método para adicionar primeira palavra na lista 
+    public void adicionaPrimeiraPalavra(String palavra) {
+        Palavra p = new Palavra(palavra);
+        p.setNext(refHead);
+        refHead = p;  
+    }
+
+
+
 }
